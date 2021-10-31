306338b9a4868f911f78ef7d8d4aed91c63555c0
vishwaschalmi <vishwaschalmi@gmail.com>
2021-10-31 15:48:49 +0530

    Viswas's first commit

---
+++ date_of_birth.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Date ");
+    scanf("%d", &a);
+    printf("Month ");
+    scanf("%d", &b);
+    printf("Year ");
+    scanf("%d", &c);
+    printf("Date of Birth %d %d %d\n",a ,b ,c);
+    return 0;
+}
